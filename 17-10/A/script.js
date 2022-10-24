e69f686ddf9c4480fbadd95b3fd00aa4779ed9de
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-10-24 20:02:13 -0300

    subindo

--- 17-10/A/script.js
+++ 17-10/A/script.js
@@ -23,13 +23,13 @@ function clicar() {
 
 function avancar() {
     window.history.go(1);
-}
 
-function voltar() {
-    window.history.go(-1);
 }
-
 //go , forward e back são asdsincronas (deve obter uma resposta primeiro)
 window.onpopstate = (e) => { //toda vez que o histórico foi modificada CAI AQUI
     document.getElementById('mensagem').innerHTML = e.state.n + 'click(s)';  //assim garante QUE O HISTORY FOI MODIFICADO
+}
+function voltar() {
+    window.history.go(-1);
+    document.getElementById('mensagem').innerHTML = window.history.state.n + 'cliques'; //obtém o estado atual
 }
