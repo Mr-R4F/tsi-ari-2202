812d3e1dc227061c088130f9d70bd926f95af05f
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-10-24 21:53:43 -0300

    Web workers & PWA

--- 24-10/T/worker.js
+++ 24-10/T/worker.js
@@ -2,9 +2,8 @@ let me = 'inicio';
 let worker = new Worker('worker.js');
 
 for (let i = 0; i < 5; i++) {
-  mes = mes + '<br>' + i;
+    document.getElementById('mensag') = mes + '<br>' + i;
 }
 
-mes = mes + '<br>' + 'fim';
-
+document.getElementById('mensag') = mes + '<br>' + 'fim';
 postMessage(me);
